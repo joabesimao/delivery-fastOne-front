a8fc46e25ab8ded5678fd73e31e102fff27d36c4
joabe simao <simaojoabecosta@gmail.com~>
2026-04-21 13:44:54 -0300

    feat: adicionar componente BorderLinearProgress estilizado

---
+++ src/components/BorderLinearProgress.tsx
@@ -0,0 +1,22 @@
+import { styled } from "@mui/material/styles";
+import { LinearProgress, linearProgressClasses } from "@mui/material";
+
+const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
+  height: 10,
+  borderRadius: 5,
+  [`&.${linearProgressClasses.colorPrimary}`]: {
+    backgroundColor: theme.palette.grey[200],
+    ...theme.applyStyles("dark", {
+      backgroundColor: theme.palette.grey[800],
+    }),
+  },
+  [`& .${linearProgressClasses.bar}`]: {
+    borderRadius: 5,
+    backgroundColor: "#006DAB",
+    ...theme.applyStyles("dark", {
+      backgroundColor: "#006DAB",
+    }),
+  },
+}));
+
+export default BorderLinearProgress;
