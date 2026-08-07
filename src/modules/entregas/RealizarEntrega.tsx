f86c0899c0b0b90b33bdcaca0ad021bc02ad0bd5
joabe simao <simaojoabecosta@gmail.com>
2026-08-07 20:14:10 -0300

    feat: ensure adjust form new client and realize one delivery

--- src/modules/entregas/RealizarEntrega.tsx
+++ src/modules/entregas/RealizarEntrega.tsx
@@ -1,13 +1,37 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import EntregaForm from "./EntregaForm";
+
+interface ClientData {
+  name: string;
+  lastName: string;
+  phone: string;
+  address: {
+    street: string;
+    neighborhood: string;
+    numberHouse: string;
+    reference: string;
+    city: string;
+  };
+}
+
+interface PreloadedData {
+  clientId: number;
+  clientData: ClientData;
+}
 
 const RealizarEntrega: React.FC = () => {
   const navigate = useNavigate();
+  const location = useLocation();
+  const preloadedData = (location.state as { clientData?: PreloadedData })?.clientData || null;
 
   return (
     <div>
-      <EntregaForm onClose={() => navigate(-1)} />
+      <EntregaForm 
+        onClose={() => navigate(-1)} 
+        preloadedClientData={preloadedData?.clientData}
+        preloadedClientId={preloadedData?.clientId}
+      />
     </div>
   );
 };
